2935055a902302b53bc0368ddb57debc56f3ea21
Spencer Neu <stneu73@gmail.com>
2024-03-18 19:23:41 -0600

    created class(camera, to store all camera and render code which will be moved from main.cpp)

---
+++ camera.h
@@ -0,0 +1,31 @@
+//
+// Created by Spencer on 3/18/2024.
+//
+
+#ifndef RAYTRACER_CAMERA_H
+#define RAYTRACER_CAMERA_H
+
+#include "rtweekend.h"
+
+#include "color.h"
+#include "hittable.h"
+
+class camera {
+public:
+    void render(const hittable& world) {
+
+    }
+
+private:
+
+    void initialize() {
+
+    }
+
+    color ray_color(const ray& r, const hittable& world) const) {
+
+    }
+};
+
+
+#endif //RAYTRACER_CAMERA_H
