be4633df9452333e196e16608a9195bbbfced071
Spencer Neu <stneu73@gmail.com>
2024-03-18 19:41:08 -0600

    refactor(moved all render and camera code into camera.h, render functions as before)

--- camera.h
+++ camera.h
@@ -10,20 +10,76 @@
 #include "color.h"
 #include "hittable.h"
 
+#include <iostream>
+#include <fstream>
+
 class camera {
 public:
-    void render(const hittable& world) {
+    double aspect_ratio;
+    int image_width;
 
+    void render(const hittable& world) {
+        initialize();
+
+        std::ofstream out("two spheres after refactor.ppm");
+
+        out << "P3\n" << image_width << ' ' << image_height << "\n255\n";
+
+        for (int j = 0; j < image_height; ++j) {
+            std::clog << "\rScanlines remaining: " << (image_height - j) << ' ' << std::flush;
+            for (int i = 0; i < image_width; ++i) {
+                auto pixel_center = pixel00_loc + (i* pixel_delta_u) + (j * pixel_delta_v);
+                auto ray_direction = pixel_center - center;
+                ray r(center, ray_direction);
+
+                color pixel_color = ray_color(r,world);
+                write_color(out, pixel_color);
+            }
+        }
+        std::clog << "\rDone.               \n";
     }
 
 private:
+    int image_height;       // rendered image height
+    point3 center;          // camera center
+    point3 pixel00_loc;    // location of pixel (0,0)
+    vec3 pixel_delta_u;     // offset to pixel to the right
+    vec3 pixel_delta_v;     // offset to pixel below
 
     void initialize() {
+        image_height = static_cast<int>(image_width / aspect_ratio);
+        image_height = (image_height < 1) ? 1 : image_height;
 
+        center = point3(0, 0, 0);
+
+        //viewport dimensions
+        auto focal_length = 1.0;
+        auto viewport_height = 2.0;
+        auto viewport_width = viewport_height * (static_cast<double>(image_width)/image_height);
+
+        // Calculate the vectors across the horizontal and down the vertical viewport edges.
+        auto viewport_u = vec3(viewport_width, 0, 0);
+        auto viewport_v = vec3(0, -viewport_height, 0);
+
+        // Calculate the horizontal and vertical delta vectors from pixel to pixel.
+        pixel_delta_u = viewport_u / image_width;
+        pixel_delta_v = viewport_v / image_height;
+
+        // Calculate the location of the upper left pixel.
+        auto viewport_upper_left = center
+                                   - vec3(0, 0, focal_length) - viewport_u/2 - viewport_v/2;
+        pixel00_loc = viewport_upper_left + 0.5 * (pixel_delta_u + pixel_delta_v);
     }
 
-    color ray_color(const ray& r, const hittable& world) const) {
+    static color ray_color(const ray& r, const hittable& world) {
+        hit_record rec;
+        if (world.hit(r,interval(0,infinity), rec)) {
+            return 0.5 * (rec.normal + color(1,1,1));
+        }
 
+        vec3 unit_direction = unit_vector(r.direction());
+        auto a = 0.5*(unit_direction.y() + 1.0);
+        return (1.0-a)*color(0.5,0.5,0.5) + a*color(0.5,0.7,1.0);
     }
 };
 
