af631b2777e044e68022a69d1effa193a676b4b9
Spencer Neu <stneu73@gmail.com>
2024-03-29 14:29:14 -0600

    bugfix(fixed lighting equation bug, r vector needed to be negative)

--- material.h
+++ material.h
@@ -5,30 +5,53 @@
 #ifndef RAYTRACER_MATERIAL_H
 #define RAYTRACER_MATERIAL_H
 
+#include "color.h"
+#include <cmath>
 
 class material {
 public:
 
-    material(double kd, double ks, double ka, double od[], double os[], double kgls) {
+    material() {
+        this->kd = 0;
+        this->ks = 0;
+        this->ka = 0;
+        od = {0,0,0};
+        os = {0,0,0};
+        this->kgls = 0;
+    }
+    material(double kd, double ks, double ka, color odiff, color ospec, double kgls) {
         this->kd = kd;
         this->ks = ks;
         this->ka = ka;
-        this->od[0] = od[0];
-        this->od[1] = od[1];
-        this->od[2] = od[2];
-        this->os[0] = os[0];
-        this->os[1] = os[1];
-        this->os[2] = os[2];
+        od = odiff;
+        os = ospec;
         this->kgls = kgls;
 
+    }
+
+    color illuminationEq(color Ia, color Ip, vec3 v, vec3 normal) {
+
+        vec3 L = {0.0,1.0,0.0};
+        color amb = Ia * ka * od;
+
+        double NdotL = dot(normal, L);
+        color diffuse = kd * Ip * od * std::max(0.0,NdotL);
+
+        vec3 r = 2*normal*dot(normal,L)-L;
+
+        double VdotR = dot(v,-r);
+        color specular = ks * os * Ip * pow(std::max(0.0,VdotR),kgls); //phong
+
+        return amb + diffuse + specular;
     }
 
     double kd;
     double ks;
     double ka;
-    double od[3];
-    double os[3];
+    color od;
+    color os;
     double kgls;
+
 };
 
 
