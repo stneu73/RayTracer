e7ff8c448027ce68a90daea5434e3e3f139cf67e
Spencer Neu <stneu73@gmail.com>
2024-04-03 21:06:23 -0600

    add(triangle shape functionality)

--- main.cpp
+++ main.cpp
@@ -7,6 +7,7 @@
 #include "hittable_list.h"
 #include "camera.h"
 #include "sphere.h"
+#include "triangle.h"
 #include "material.h"
 
 int main() {
@@ -41,35 +42,41 @@ int main() {
 //    color lightSource{1.0,1.0,1.0};
 //    color background{0.2,0.2,0.2};
 
-    //scene 3 the snowman
-    //base
-    world.add(make_shared<sphere>(point3(0,-0.75,0.0),.32,make_shared<material>(
-            0.7, 0.2, 0.1, color{1.0, 1.0, 1.0}, color{1.0, 1.0, 1.0}, 1.0)));
-    //body
-    world.add(make_shared<sphere>(point3(0,-0.33,0.0),.23,make_shared<material>(
-            0.7, 0.2, 0.1, color{1.0, 1.0, 1.0}, color{1.0, 1.0, 1.0}, 1.0)));
-    //head
-    world.add(make_shared<sphere>(point3(0,0.01,0),.16,make_shared<material>(
-            0.7, 0.2, 0.1, color{1.0, 1.0, 1.0}, color{1.0, 1.0, 1.0}, 1.0)));
-    //eyes
-    world.add(make_shared<sphere>(point3(-.05,0.03,0.16),.01,make_shared<material>(
-            0.7, 0.2, 0.1, color{0, 0, 0}, color{0.3, 0, 0}, 64.0)));
-    world.add(make_shared<sphere>(point3(.05,0.03,0.16),.01,make_shared<material>(
-            0.7, 0.2, 0.1, color{0, 0, 0}, color{0.3, 0, 0}, 64.0)));
-    //nose
-//    world.add(make_shared<sphere>(point3(0,0.01,0.16),.01,make_shared<material>(
+//    //scene 3 the snowman
+//    //base
+//    world.add(make_shared<sphere>(point3(0,-0.75,0.0),.32,make_shared<material>(
+//            0.7, 0.2, 0.1, color{1.0, 1.0, 1.0}, color{1.0, 1.0, 1.0}, 1.0)));
+//    //body
+//    world.add(make_shared<sphere>(point3(0,-0.33,0.0),.23,make_shared<material>(
+//            0.7, 0.2, 0.1, color{1.0, 1.0, 1.0}, color{1.0, 1.0, 1.0}, 1.0)));
+//    //head
+//    world.add(make_shared<sphere>(point3(0,0.01,0),.16,make_shared<material>(
+//            0.7, 0.2, 0.1, color{1.0, 1.0, 1.0}, color{1.0, 1.0, 1.0}, 1.0)));
+//    //eyes
+//    world.add(make_shared<sphere>(point3(-.05,0.03,0.16),.01,make_shared<material>(
+//            0.7, 0.2, 0.1, color{0, 0, 0}, color{0.3, 0, 0}, 64.0)));
+//    world.add(make_shared<sphere>(point3(.05,0.03,0.16),.01,make_shared<material>(
+//            0.7, 0.2, 0.1, color{0, 0, 0}, color{0.3, 0, 0}, 64.0)));
+//    //nose
+////    world.add(make_shared<sphere>(point3(0,0.01,0.16),.01,make_shared<material>(
+////            0.7, 0.2, 0.1, color{0, 0, 0}, color{0.3, 0, 0}, 100.0)));
+//    //mouth
+//    world.add(make_shared<sphere>(point3(0,-0.04,0.16),.014,make_shared<material>(
 //            0.7, 0.2, 0.1, color{0, 0, 0}, color{0.3, 0, 0}, 100.0)));
-    //mouth
-    world.add(make_shared<sphere>(point3(0,-0.04,0.16),.014,make_shared<material>(
-            0.7, 0.2, 0.1, color{0, 0, 0}, color{0.3, 0, 0}, 100.0)));
-    //buttons
-    world.add(make_shared<sphere>(point3(0,-0.23,0.21),.03,make_shared<material>(
-            0.7, 0.2, 0.1, color{0, 0, 0}, color{0.3, 0, 0}, 1.0)));
-    world.add(make_shared<sphere>(point3(0,-0.4,0.21),.03,make_shared<material>(
-            0.7, 0.2, 0.1, color{0, 0, 0}, color{0.3, 0, 0}, 1.0)));
-    //floor
-    world.add(make_shared<sphere>(point3(0,-10100,-1),10000,make_shared<material>(
-            0.5, 0, 0.1, color{.2, .2, .2}, color{0.3, .2, .2}, 1.0)));
+//    //buttons
+//    world.add(make_shared<sphere>(point3(0,-0.23,0.21),.03,make_shared<material>(
+//            0.7, 0.2, 0.1, color{0, 0, 0}, color{0.3, 0, 0}, 1.0)));
+//    world.add(make_shared<sphere>(point3(0,-0.4,0.21),.03,make_shared<material>(
+//            0.7, 0.2, 0.1, color{0, 0, 0}, color{0.3, 0, 0}, 1.0)));
+//    //floor
+//    world.add(make_shared<sphere>(point3(0,-10100,-1),10000,make_shared<material>(
+//            0.5, 0, 0.1, color{.2, .2, .2}, color{0.3, .2, .2}, 1.0)));
+    world.add(make_shared<triangle>(point3(-1,-1,0),
+                                    point3(1,-1,0),
+                                    point3(0,1,0),
+                                    make_shared<material>(.9,1.0,0.1,
+                                                          color(1.0,1.0,0.0),
+                                                          color(1.0,1.0,1.0),4.0)));
     vec3 lightDir = {-1.0,0.0,1.0};
     color ambient{0.1,0.1,0.1};
     color lightSource{1.0,1.0,1.0};
@@ -79,7 +86,7 @@ int main() {
     //initialize camera
     camera cam;
 
-    cam.filename = "scene 3 render.ppm";
+    cam.filename = "first triangle.ppm";
     cam.aspect_ratio = 1;
     cam.image_width = 400;
     cam.vfov = 65;
---
+++ triangle.h
@@ -0,0 +1,81 @@
+//
+// Created by Spencer on 4/3/2024.
+//
+
+#ifndef RAYTRACER_TRIANGLE_H
+#define RAYTRACER_TRIANGLE_H
+
+
+#include <cmath>
+
+#include "hittable.h"
+#include "vec3.h"
+
+class triangle : public hittable {
+public:
+    triangle(point3 a, point3 b, point3 c, const shared_ptr<material>& _mat) : v0(a), v1(b), v2(c),mat(_mat->kd,_mat->ks,_mat->ka,_mat->od,_mat->os,_mat->kgls){}
+
+    bool hit(const ray& r, interval(ray_t), hit_record& rec) const override {
+        // Compute the plane's normal
+        vec3 v0v1 = v1 - v0;
+        vec3 v0v2 = v2 - v0;
+        // No need to normalize
+        vec3 N = cross(v0v1,v0v2); // N
+//        double area2 = N.length();
+
+        // Step 1: Finding P
+
+        // Check if the ray and plane are parallel
+        double NdotRayDirection = dot(N,r.direction());
+        if (std::fabs(NdotRayDirection) < 0) // Almost 0
+            return false; // They are parallel, so they don't intersect!
+
+        // Compute d parameter using equation 2
+        double d = dot(-N,v0);
+
+        // Compute t (equation 3)
+        double t = -(dot(N,r.origin()) + d) / NdotRayDirection;
+
+        // Check if the triangle is behind the ray
+        if (!ray_t.surrounds(t)) return false; // The triangle is behind
+
+        // Compute the intersection point using equation 1
+        vec3 P = r.origin() + t * r.direction();
+
+        // Step 2: Inside-Outside Test
+        vec3 C; // Vector perpendicular to triangle's plane
+
+        // Edge 0
+        vec3 edge0 = v1 - v0;
+        vec3 vp0 = P - v0;
+        C = cross(edge0,vp0);
+        if (dot(N,C) < 0) return false; // P is on the right side
+
+        // Edge 1
+        vec3 edge1 = v2 - v1;
+        vec3 vp1 = P - v1;
+        C = cross(edge1,vp1);
+        if (dot(N,C) < 0) return false; // P is on the right side
+
+        // Edge 2
+        vec3 edge2 = v0 - v2;
+        vec3 vp2 = P - v2;
+        C = cross(edge2,vp2);
+        if (dot(N,C) < 0) return false; // P is on the right side
+
+        return true; // This ray hits the triangle
+    }
+
+    material getMat() override {
+        return mat;
+    }
+
+private:
+    point3 v0;
+    point3 v1;
+    point3 v2;
+    material mat;
+};
+
+
+#endif //RAYTRACER_TRIANGLE_H
